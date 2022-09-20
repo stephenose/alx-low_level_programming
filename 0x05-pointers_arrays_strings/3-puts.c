ee44bb871778c1234100f6163f4d5f6acaac1805
stephenose <saturdaystephen1@gmail.com>
2022-09-19 18:11:56 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - function that prints a string followed by a new line
+ * @str: is the string
+ * Return 0
+ */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		putchar(str[n]);
+		n++;
+	}
+	putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
